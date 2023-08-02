915b97da7ea3ecac0998bd5e64dd2b252d80c52f
muhammadcahya <muhammadcahyax@gmail.com>
2023-08-02 09:01:36 +0700

    Add log for IR draft

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -126,6 +126,7 @@ class InvoiceController extends Controller
         try {
             $this->repository::save($request);
         } catch (Throwable $exception) {
+            info($exception);
             return redirect()->back()->withErrors([
                 'error' => __('messages.error.internal_server'),
                 'exception' => $exception->getMessage()
@@ -150,6 +151,7 @@ class InvoiceController extends Controller
         try {
             $this->repository::store($request);
         } catch (Throwable $exception) {
+            info($exception);
             return redirect()->back()->withErrors([
                 'error' => __('messages.error.internal_server'),
                 'exception' => $exception->getMessage()
@@ -232,6 +234,7 @@ class InvoiceController extends Controller
         try {
             return $this->repository::init($invoice)->revision($expense);
         } catch (Throwable $exception) {
+            info($exception);
             return redirect()->back()->withErrors([
                 'error' => __('messages.error.internal_server'),
                 'exception' => $exception->getMessage()
@@ -268,6 +271,7 @@ class InvoiceController extends Controller
         try {
             $this->repository::init($invoice)->update($request);
         } catch (Throwable $exception) {
+            info($exception);
             return redirect()->back()->withErrors([
                 'error' => __('messages.error.internal_server'),
                 'exception' => $exception->getMessage()
