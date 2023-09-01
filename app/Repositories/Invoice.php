51bfbccc8b6a9ea1ffaab68817313b698c5b6fa9
muhammadcahya <muhammadcahyax@gmail.com>
2023-09-01 14:47:03 +0700

    reverse

--- app/Repositories/Invoice.php
+++ app/Repositories/Invoice.php
@@ -469,8 +469,8 @@ class Invoice
     public function deltaValidate(): Model
     {
         if ($this->model->smuItems) {
-            // $smuItems = $this->model->smuItems->where('validation_score', '!=', 5);
-            $smuItems = $this->model->smuItems->where('is_validated', false);
+            $smuItems = $this->model->smuItems->where('validation_score', '!=', 5);
+            // $smuItems = $this->model->smuItems->where('is_validated', false);
             foreach ($smuItems as $item) {
                 $amount = $item->amount;
                 $tax = 0;
@@ -483,7 +483,7 @@ class Invoice
                     $amountAfterTax = $amount + $tax - $withholding;
                 }
                 $item->update([
-                    // 'is_validated' => false,
+                    'is_validated' => false,
                     'vat_tax' => $tax,
                     'withholding_tax' => $withholding,
                     'amount_after_tax' => $amountAfterTax
@@ -493,8 +493,8 @@ class Invoice
         }
 
         if ($this->model->awbItems) {
-            // $awbItems = $this->model->awbItems->where('validation_score', '!=', 5)->where('uuid', '!=', null);
-            $awbItems = $this->model->awbItems->where('is_validated', false)->where('uuid', '!=', null);
+            $awbItems = $this->model->awbItems->where('validation_score', '!=', 5)->where('uuid', '!=', null);
+            // $awbItems = $this->model->awbItems->where('is_validated', false)->where('uuid', '!=', null);
             foreach ($awbItems as $item) {
                 $amount = $item->amount;
                 $tax = 0;
@@ -507,7 +507,7 @@ class Invoice
                     $amountAfterTax = $amount + $tax - $withholding;
                 }
                 $item->update([
-                    // 'is_validated' => false,
+                    'is_validated' => false,
                     'vat_tax' => $tax,
                     'withholding_tax' => $withholding,
                     'amount_after_tax' => $amountAfterTax
