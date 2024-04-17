f49b604ddb8d921e25ad154cc8a948383e654326
muhammadcahya <muhammadcahyax@gmail.com>
2024-04-17 16:33:59 +0700

    Fix bug on transfer method

--- app/Repositories/Approval.php
+++ app/Repositories/Approval.php
@@ -84,7 +84,7 @@ class Approval
                         'ap_source' => 'RAPSYS',
                         'terms_id' => $this->model->term->code,
                         'invoice_type_lookup_code' => strtoupper($this->model->invoiceType->name),
-                        'payment_method_lookup_code' => 'TRANSFER',
+                        'payment_method_lookup_code' => 'Transfer',
                         'status' => 'I',
                     ]);
 
